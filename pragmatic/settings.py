f0aa12e6df8a699cec04279fe05f1e2dd7768dcc
ksb-hub <gayun53a@naver.com>
2023-04-11 00:12:36 +0900

    course 11

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -129,3 +129,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
