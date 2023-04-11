ae75f83358a93aa735cc70710d25efc649d9a464
ksb-hub <gayun53a@naver.com>
2023-04-11 19:27:51 +0900

    course 17

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
 
 # Create your views here.
 def hello_world(requset):
-    return render(requset, 'accountapp/hello_world.html')
+    if requset.method == 'POST':
+
+        temp = requset.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(requset, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+    else:
+        return render(requset, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
